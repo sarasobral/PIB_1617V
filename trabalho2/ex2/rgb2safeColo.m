f7b86f46775600081c3bd434e635cf4f14b44d54
sarasobral <40602@alunos.isel.ipl.pt>
2017-06-08 20:48:32 +0100

    finished exercise 1, starting exercise 2

---
+++ trabalho2/ex2/rgb2safeColo.m
@@ -0,0 +1,22 @@
+%
+% ISEL - Instituto Superior de Engenharia de Lisboa.
+%
+% LEIC - Licenciatura em Engenharia Informatica e de Computadores.
+% MEIC - Mestrado em Engenharia Informatica e de Computadores.
+%
+% PIB - Processamento de Imagem e Biometria.
+%
+
+
+function distinct_colors = rgb2safeColo(safe)
+    
+    if nargin==0
+        safe=1          
+    end
+%   web safe color
+    if(safe==1)
+%   web safest color
+    else
+    end
+
+end
