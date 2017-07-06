a4a1e5332ec156b93fe2342554737797a9057813
sarasobral <40602@alunos.isel.ipl.pt>
2017-07-06 15:07:56 +0100

    exercise 4: extraction of features in vector format to use on matlab

--- trabalho2/ex4/FacialRecognition/FacialRecognition/Program.cs
+++ trabalho2/ex4/FacialRecognition/FacialRecognition/Program.cs
@@ -6,26 +6,24 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Net.Http.Headers;
 using System.Net.Http;
-
+using System.Xml.Linq;
 
 namespace FacialRecognition {
     class Program {
         private static readonly String path1 = "..\\..\\..\\FaceImages\\";
         private static readonly String path2 = "..\\..\\..\\RealFaceImages\\";
-        private static readonly String[] bd_images = { "face1.bmp", "face2.bmp", "face3.bmp", "face4.bmp", "face5.bmp" };
-        private static readonly String[] real_images = { "bebe.jpg", "george.jpg", "samuel.jpg", "sara.jpg" };
-
+        private static readonly String path3 = "..\\..\\..\\CaltechFaceDatabase\\";
+        
         static void Main(string[] args)
         {
             String option;
             do
             {
-                Console.Write("(a) Extrair caracteristicas\n"+
-                    "(b) Identificar uma pessoa\n"+
+                CleanUp();
+                Console.Write("(a) Extrair caracteristicas\n" +
                     "(exit) para encerrar a aplicação\n");
                 option = Console.ReadLine();
                 if (option == "a") { CleanUp(); ExtractFeatures(); }
-                else if (option == "b") { CleanUp(); RunClassifier(); }
                 else if (option == "exit") break;
             } while (true);
         }
@@ -66,18 +64,8 @@ namespace FacialRecognition {
 
             //A peak at the JSON response.
             if (file != "") file += ": ";
-            Console.WriteLine(file + responseContent);
-            String filename = imageFilePath + ".json";
-            System.IO.StreamWriter writer = new System.IO.StreamWriter(filename);
-            writer.WriteLine(responseContent);
-            writer.Close();
-
-        }
-
-        static void MakeDetectRequestFor(String path, String[] images, String queryString)
-        {
-            for (int i = 0; i < images.Length; ++i)
-                MakeDetectRequest(path + images[i], images[i], queryString);
+           // Console.WriteLine(file + responseContent);
+            ExtractObject(imageFilePath, responseContent);
         }
 
         static String ChooseFeatures()
@@ -145,35 +133,58 @@ namespace FacialRecognition {
             String option, queryString;
             do
             {
-                Console.Write("(a) Extrair características de uma base de dados especifica\n"+
-                    "(b) Extrair características de imagens reais\n"+
-                    "(c) Inserir um caminho à escolha\n"+
-                    "(home) para voltar ao início\n"+
+                Console.Write("(a) Extrair características das imagens de FaceImages\n" +
+                    "(b) Extrair características de imagens reais\n" +
+                    "(c) Extrair características das imagens de CaltechFaceDatabase\n" +
+                    "(home) para voltar ao início\n" +
                     "(exit) para encerrar a aplicação\n");
                 option = Console.ReadLine();
                 if (option == "home") { CleanUp(); return; }
                 else if (option == "exit") System.Environment.Exit(1);
                 queryString = ChooseFeatures(); // Request parameters
-                if (option == "a") MakeDetectRequestFor(path1, bd_images, queryString);
-                else if (option == "b") MakeDetectRequestFor(path2, real_images, queryString);
-                else MakeDetectRequest(option, Path.GetFileName(option), queryString);
+                if (option == "a") MakeDetectRequestFor(path1, queryString);
+                else if (option == "b") MakeDetectRequestFor(path2, queryString);
+                else MakeDetectRequestFor(path3, queryString); ;
 
                 Console.WriteLine("\n\n\nEspere pelos resultados ou vá à diretoria e consulte os ficheiro .txt gerados...\n\n\n");
                 Console.ReadLine();
             } while (true);
         }
-
-        static void RunClassifier() {
-            String option;
-            do
-            {
-                Console.Write("(home) para voltar ao início\n" +
-                        "(exit) para encerrar a aplicação\n");
-                option = Console.ReadLine();
-                if (option == "home") { CleanUp();  return; }
-                else if(option == "exit") System.Environment.Exit(1);
-            } while (true);
+        static int count = 1;
+        private static void MakeDetectRequestFor(String path, String queryString)
+        {
+            String[] images = Directory.GetFiles(path, "*.jpg")
+                .Select(Path.GetFileName)
+                .ToArray();
+            if(images.Length==0)
+                images = Directory.GetFiles(path, "*.bmp")
+                    .Select(Path.GetFileName)
+                    .ToArray();
+            for (int i = 0; i < images.Length; ++i)
+                    MakeDetectRequest(path + images[i], images[i], queryString);
         }
+
+        static void ExtractObject(string imageFilePath, string responseContent)
+        {
+            string[] values = responseContent.Split(',',':');
+            string finalValues = "";
+            foreach (string value in values)
+                if (Char.IsDigit(value.ToCharArray()[0]))
+                {
+                    string val = value.Replace("}", string.Empty).Replace("]", string.Empty);
+                    finalValues+=val+",";
+                }
+            string[] aux = imageFilePath.Split('\\','.');
+            string au = aux[aux.Length-2];
+            finalValues += au;
+
+            String filename = imageFilePath + ".txt";
+            System.IO.StreamWriter writer = new System.IO.StreamWriter(filename);
+            writer.WriteLine(finalValues);
+            writer.Close();
+
+        }
+
         static void CleanUp()
         {
             Console.Clear();
